479ebba05e5444daaefd3839f1648c37d0be5b8f
unnikrishnanbiju <unni1995biju@gmail.com>
2025-03-10 08:12:28 +0530

    second commit

---
+++ src/ex_01_Java_Basic/Practice.java
@@ -0,0 +1,25 @@
+package ex_01_Java_Basic;
+
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the number");
+
+        int num = sc.nextInt();
+
+        int rev= 0;
+
+        while(num !=0)
+        {
+            rev= rev*10 + num%10;
+            num = num/ 10;
+        }
+        System.out.println("the reversed number is " +rev);
+
+
+
+   }
+}
