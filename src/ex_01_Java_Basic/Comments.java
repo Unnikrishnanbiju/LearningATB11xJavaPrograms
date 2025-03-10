516fb5e2f4cdb04e1afb5d3527c497a4cddedf99
unnikrishnanbiju <unni1995biju@gmail.com>
2025-03-10 12:49:00 +0530

    3 commit

---
+++ src/ex_01_Java_Basic/Comments.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Comments {
+    public static void main(String[] args) {
+//        Adding the comment
+
+        System.out.println("Comment added");
+    }
+}
