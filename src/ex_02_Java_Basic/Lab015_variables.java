0518e273066d4f334c430765665f68196bdc38f9
unnikrishnanbiju <unni1995biju@gmail.com>
2025-03-11 07:28:29 +0530

    4th commit 5th commit

---
+++ src/ex_02_Java_Basic/Lab015_variables.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basic;
+
+public class Lab015_variables {
+    public static void main(String[] args) {
+
+//        data_type variable_name = varible_value
+        byte b = 10;
+        System.out.println(b);
+    }
+}
