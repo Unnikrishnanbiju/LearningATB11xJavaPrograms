9d41ffb37158b25cd9d6dad9c5ea89ad3b37e1eb
unnikrishnanbiju <unni1995biju@gmail.com>
2025-03-11 13:01:41 +0530

    4th commit 5th commit

---
+++ src/ex_02_Java_Basic/Lab016_Java_TableOfNine.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Basic;
+
+public class Lab016_Java_TableOfNine {
+    public static void main(String[] args) {
+        int n = 9;
+
+        System.out.printf("Table of " +n+ "%n");
+        System.out.printf("%d * 1 = %d %n",n,(n));
+        System.out.printf("%d * 1 = %d %n",n,(n*2));
+        System.out.printf("%d * 1 = %d %n",n,(n*3));
+        System.out.printf("%d * 1 = %d %n",n,(n*4));
+        System.out.printf("%d * 1 = %d %n",n,(n*5));
+        System.out.printf("%d * 1 = %d %n",n,(n*6));
+        System.out.printf("%d * 1 = %d %n",n,(n*7));
+        System.out.printf("%d * 1 = %d %n",n,(n*8));
+        System.out.printf("%d * 1 = %d %n",n,(n*9));
+        System.out.printf("%d * 1 = %d %n",n,(n*10));
+    }
+}
