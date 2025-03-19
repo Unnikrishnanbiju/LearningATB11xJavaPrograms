cf39d9ddca45daebf648a33977f8cac2fe82bfe8
unnikrishnanbiju <unni1995biju@gmail.com>
2025-03-19 08:14:24 +0530

    day12

---
+++ src/ex_08_Loop/Table_9.java
@@ -0,0 +1,12 @@
+package ex_08_Loop;
+
+public class Table_9 {
+    public static void main(String[] args) {
+        System.out.println("table of 9");
+        int number = 9;
+        for (int i = 1; i <= 10; i++){
+            System.out.println(number +"X"+i+"="+(number*i));
+
+        }
+    }
+}
