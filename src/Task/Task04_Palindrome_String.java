14952ef2599b9cc57b384cef4e580ffd355c724e
unnikrishnanbiju <unni1995biju@gmail.com>
2025-03-23 20:51:31 +0530

    day12

---
+++ src/Task/Task04_Palindrome_String.java
@@ -0,0 +1,26 @@
+package Task;
+
+import java.util.Scanner;
+
+public class Task04_Palindrome_String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the word to check palindrome or not ?");
+        String str = sc.next();
+        String str_rev = "";
+        for (int i = str.length()-1; i >=0 ; i--) {
+            str_rev = str_rev + str.charAt(i);
+        }
+        System.out.println(str_rev);
+        if (str.equalsIgnoreCase(str_rev)) {
+
+            System.out.println("It is palindrome");
+        }
+
+            else{
+                System.out.println("Its not palindrome");
+            }
+
+
+    }
+}
