42b722308486a6bb83cfaa16eae7c39d3703faaf
unnikrishnanbiju <unni1995biju@gmail.com>
2025-03-28 21:06:51 +0530

    day12

---
+++ src/ex_12_Strings/Lab136_string_Imm.java
@@ -0,0 +1,9 @@
+package ex_12_Strings;
+
+public class Lab136_string_Imm {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        s1 = s1.concat("world");
+        System.out.println(s1);
+    }
+}
---
+++ src/ex_12_Strings/Lab137_string_functions.java
@@ -0,0 +1,14 @@
+package ex_12_Strings;
+
+public class Lab137_string_functions {
+    public static void main(String[] args) {
+        char c = 'A';
+        System.out.println(c);
+
+        String s1 = "ABCD";
+        System.out.println(s1.length());
+        System.out.println(s1.toLowerCase());
+        System.out.println(s1.concat("E"));
+
+    }
+}
