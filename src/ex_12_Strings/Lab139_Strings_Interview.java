73b7299189f59bd99c08b1b757a24dede0a2ebf0
unnikrishnanbiju <unni1995biju@gmail.com>
2025-03-28 21:17:09 +0530

    day12

---
+++ src/ex_12_Strings/Lab139_Strings_Interview.java
@@ -0,0 +1,9 @@
+package ex_12_Strings;
+
+public class Lab139_Strings_Interview {
+    public static void main(String[] args) {
+        String name = "unnikrishnan";
+        name = name.toUpperCase();
+        System.out.println(name);
+    }
+}
