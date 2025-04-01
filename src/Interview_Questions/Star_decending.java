70e9b3cba35bd5c52c9d01f2f2f61120bb0cecdd
unnikrishnanbiju <unni1995biju@gmail.com>
2025-04-01 13:30:47 +0530

    day12

---
+++ src/Interview_Questions/Star_decending.java
@@ -0,0 +1,19 @@
+package Interview_Questions;
+
+import java.util.Scanner;
+
+public class Star_decending {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        for (int i = n; i >=1 ; i--) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+            
+        }
+    }
+}
