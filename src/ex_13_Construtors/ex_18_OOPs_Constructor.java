01ee81d0b04cc73c0d8f333712f56d30a9c5ebe8
unnikrishnanbiju <unni1995biju@gmail.com>
2025-04-03 20:40:53 +0530

    day12

---
+++ src/ex_13_Construtors/ex_18_OOPs_Constructor.java
@@ -0,0 +1,30 @@
+package ex_13_Construtors;
+
+public class ex_18_OOPs_Constructor {
+
+    public static void main(String[] args) {
+        Baby b1 = new Baby(); // ✅ Now works fine
+        b1.cry();
+    }
+
+    static class Baby { // ✅ Making it static
+
+        String name;
+
+        void cry() {
+            System.out.println("Cry!!");
+        }
+
+        void sleep() {
+            System.out.println("Sleep!!");
+        }
+
+        void eat() {
+            System.out.println("Eat!!");
+        }
+
+        Baby() {
+            System.out.println("I'm a default constructor");
+        }
+    }
+}
