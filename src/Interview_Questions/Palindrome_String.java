248584be0270d4e9e85576dd89fc8612b71d6611
unnikrishnanbiju <unni1995biju@gmail.com>
2025-04-09 20:39:39 +0530

    day12

--- src/Interview_Questions/Palindrome_String.java
+++ src/Interview_Questions/Palindrome_String.java
@@ -1,4 +1,28 @@
 package Interview_Questions;
 
+import java.util.Scanner;
+
 public class Palindrome_String {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String num = sc.next();
+        String org_num = num;
+         String rev = "";
+
+         int len = num.length();
+        for(int i = len-1; i >=0 ; i--) {
+            rev = rev + num.charAt(i);
+        }
+            if (org_num.equals(rev)){
+                System.out.println("Palindrome");
+            }
+            else {
+                System.out.println("NOt palindrome");
+            }
+
+
+        }
+
+    }
+
--- src/Interview_Questions/swapping_two_number.java
+++ src/Interview_Questions/swapping_two_number.java
@@ -1,4 +1,25 @@
 package Interview_Questions;
 
+import java.util.Scanner;
+
 public class swapping_two_number {
+    public static void main(String[] args) {
+
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the numebrs");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+int c = 0;
+c = a;
+a = b;
+b= c;
+
+//        a = a+b;
+//        b= a-b;
+//        a = a-b;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
