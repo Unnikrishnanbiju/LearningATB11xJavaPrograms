6a7f021251a9742fc8fc7e616f0db163af803ec3
unnikrishnanbiju <unni1995biju@gmail.com>
2025-04-19 22:12:08 +0530

    17-4

---
+++ src/ForLoop/multiplication_07.java
@@ -0,0 +1,13 @@
+package ForLoop;
+
+public class multiplication_07 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int num = 7;
+        while(i <= 10){
+            System.out.println(num +" X " + i +" = " +(num * i));
+            i++;
+        }
+    }
+}
