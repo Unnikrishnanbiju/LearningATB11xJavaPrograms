c89da3adf949e14aaf0e8e212241704052af30da
unnikrishnanbiju <unni1995biju@gmail.com>
2025-04-21 17:59:29 +0530

    21-4

--- src/Interview_Questions/reverse_Array.java
+++ src/Interview_Questions/reverse_Array.java
@@ -8,7 +8,7 @@ public class reverse_Array {
         for (int i = words.length - 1 ; i >= 0 ; i--) {
             System.out.print(words[i]);
             if ( i != 0){
-                System.out.printf(" ");
+                System.out.println(" ");
             }
         }
     }
