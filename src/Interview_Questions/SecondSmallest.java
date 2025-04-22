8ef646d3f9bce010224c3c6bb89b1cbdde79987f
unnikrishnanbiju <unni1995biju@gmail.com>
2025-04-22 21:08:02 +0530

    22-4

--- src/Interview_Questions/SecondSmallest.java
+++ src/Interview_Questions/SecondSmallest.java
@@ -10,6 +10,7 @@ public class SecondSmallest {
         int secondSamllest = arr[1];
         int secondLargest = arr[arr.length -2];
 
+        System.out.println(Arrays.toString(arr));
         System.out.println(secondSamllest);
         System.out.println(secondLargest);
 
