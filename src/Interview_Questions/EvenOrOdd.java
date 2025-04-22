578ace16f57a3dc4601dee885a6b2e1149ed8239
unnikrishnanbiju <unni1995biju@gmail.com>
2025-04-22 12:01:09 +0530

    22-4

---
+++ src/Interview_Questions/EvenOrOdd.java
@@ -0,0 +1,18 @@
+package Interview_Questions;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0){
+            System.out.println("EvenNumber");
+        }
+        else {
+            System.out.println("Odd Number");
+        }
+    }
+}
